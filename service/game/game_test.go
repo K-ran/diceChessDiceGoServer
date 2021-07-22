e1a5494e7a20ae23a788a6416b69db7c26e9fa18
Karan Purohit <karnpurohit@gmail.com>
2021-07-22 18:46:04 +0000

    Added experimental test

---
+++ service/game/game_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestNewGame(t *testing.T) {
+	game := NewDiceChessGame("abcd", "Ram", "Shyam", 3)
+	if value, err := json.Marshal(game); err == nil {
+		compareString := "{\"p1\":{\"name\":\"Ram\"},\"p2\":{\"name\":\"Shyam\"},\"gameID\":\"abcd\",\"dice\":[{\"value\":0},{\"value\":0},{\"value\":0}]}"
+		if compareString != string(value) {
+			t.Fatal("Comparision failed, expected {}\n got {}", compareString, string(value))
+		}
+	} else {
+		t.Fatal("Failed to marsh game object")
+	}
+}
