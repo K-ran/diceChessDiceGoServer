08a26832becf87d5cdeb32af2513d0f4a9b1e4d9
Karan Purohit <karnpurohit@gmail.com>
2021-07-31 12:25:37 +0000

    Added server prefix code

--- internal/storewrapper/game_store_api.go
+++ internal/storewrapper/game_store_api.go
@@ -2,6 +2,9 @@ package storewrapper
 
 import (
 	"crypto/rand"
+	"os"
+	"strconv"
+	"strings"
 	"sync"
 
 	"math/big"
@@ -9,25 +12,25 @@ import (
 	"github.com/k-ran/diceChessDiceServer/pkg/keyvaluestore"
 )
 
+const GAME_ID_LENGTH = 8
+const PLAYER_ID_LENGTH = 32
+const LETTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_"
+
 // Generates base64 random string of length n
 func generateRandomString(n int) (string, error) {
-	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_"
 	ret := make([]byte, n)
 	for i := 0; i < n; i++ {
-		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(LETTERS))))
 		if err != nil {
 			return "", err
 		}
-		ret[i] = letters[num.Int64()]
+		ret[i] = LETTERS[num.Int64()]
 	}
 
 	return string(ret), nil
 }
 
 var player_mu sync.Mutex
-
-const GAME_ID_LENGTH = 7
-const PLAYER_ID_LENGTH = 32
 
 //Generates a unique player id in store
 func GreateUniquePlayerId(store keyvaluestore.KeyValueStore, gameId string) string {
@@ -50,10 +53,24 @@ func GreateUniquePlayerId(store keyvaluestore.KeyValueStore, gameId string) stri
 
 var game_mu sync.Mutex
 
+// Generated game prefix based on Env variable
+func getRandomServerPrefix() string {
+	prefix := os.Getenv("GO_SERVER_PEFIX")
+	if prefix == "" {
+		prefix = "0-63"
+	}
+
+	start, _ := strconv.Atoi(strings.Split(prefix, "-")[0])
+	end, _ := strconv.Atoi(strings.Split(prefix, "-")[1])
+	num, _ := rand.Int(rand.Reader, big.NewInt(int64(end-start+1)))
+	prefixString := string(LETTERS[start+int(num.Int64())])
+	return prefixString
+}
+
 //Generates a unique game id in store
 //TODO: add another prefix character based in env variable GO_SEVER_PREFIX
 func GreateUniqueGameId(store keyvaluestore.KeyValueStore) string {
-	value, _ := generateRandomString(GAME_ID_LENGTH)
+	value, _ := generateRandomString(GAME_ID_LENGTH - 1)
 	game_mu.Lock()
 	defer game_mu.Unlock()
 
@@ -61,10 +78,11 @@ func GreateUniqueGameId(store keyvaluestore.KeyValueStore) string {
 
 	//keep trying till we get a unique key
 	for err == nil {
-		value, _ = generateRandomString(GAME_ID_LENGTH)
+		value, _ = generateRandomString(GAME_ID_LENGTH - 1)
 		_, err = store.Get(value)
 	}
-
+	prefix := getRandomServerPrefix()
+	value = prefix + value
 	store.Set(value, "{}", 0)
 
 	return value
