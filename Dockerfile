aea2cf529c9625be1cd13525ba714aa54a80eabe
Karan Purohit <karnpurohit@gmail.com>
2021-08-22 17:39:08 +0000

    Api root changes.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /go_server
 RUN  apt-get update \
   && apt-get install -y wget \
   && rm -rf /var/lib/apt/lists/*
-RUN wget https://github.com/K-ran/diceChessDiceGoServer/releases/download/v1/dcd_server
+RUN wget https://github.com/K-ran/diceChessDiceGoServer/releases/download/latest/dcd_server
 RUN chmod +x dcd_server
 RUN echo "redis-server --daemonize yes" >> start.sh
 RUN echo "./dcd_server" >> start.sh
--- cmd/main.go
+++ cmd/main.go
@@ -20,10 +20,10 @@ func main() {
 	gamehandler.InitDb()
 	r := mux.NewRouter()
 
-	r.HandleFunc("/create/{playerName}/{gameName}/{dieNum}", gamehandler.GetCreateHttpHandler()).Methods("POST", "OPTIONS")
-	r.HandleFunc("/join/{gameId}/{playerName}", gamehandler.GetJoinHandler()).Methods("POST")
-	r.HandleFunc("/getstatus/{gameId}/{playerId}", gamehandler.GetGetStatusHandler()).Methods("GET")
-	r.HandleFunc("/roll/{gameId}/{playerId}", gamehandler.GetRollHandler()).Methods("GET")
+	r.HandleFunc("/api/v1/create/{playerName}/{gameName}/{dieNum}", gamehandler.GetCreateHttpHandler()).Methods("POST", "OPTIONS")
+	r.HandleFunc("/api/v1/join/{gameId}/{playerName}", gamehandler.GetJoinHandler()).Methods("POST")
+	r.HandleFunc("/api/v1/getstatus/{gameId}/{playerId}", gamehandler.GetGetStatusHandler()).Methods("GET")
+	r.HandleFunc("/api/v1/roll/{gameId}/{playerId}", gamehandler.GetRollHandler()).Methods("GET")
 	http.Handle("/", r)
 
 	address := os.Getenv("GO_PORT")
